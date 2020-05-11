f5ba5597e465e16a2684707fd89298261d7114de
fowwo <54595444+fowwo@users.noreply.github.com>
2020-05-10 21:26:57 -0500

    Add info icon

--- output.js
+++ output.js
@@ -93,14 +93,24 @@ function outputText(message) {
 	const table = document.getElementById("log");
 
 	let row = document.createElement("tr");
-	row.appendChild(document.createElement("td"));
 
 	let cell = document.createElement("td");
+	cell.style = "vertical-align: top;";
+
+	let img = document.createElement("img");
+	img.className = "icon";
+	img.style = "box-shadow: 0 0 50px #fff;";
+	img.src = "img/info.png";
+	img.alt = "Info";
+	cell.appendChild(img);
+
+	row.appendChild(cell);
+
+	cell = document.createElement("td");
 	cell.style = "padding: 0 5px;";
 	cell.innerHTML = message;
 	row.appendChild(cell);
 
 	table.appendChild(row);
-	
 
 }
